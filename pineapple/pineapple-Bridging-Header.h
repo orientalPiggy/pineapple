7a21e535e44053b84d991c98c399916757071d2c
orientalPiggy <40441924+orientalPiggy@users.noreply.github.com>
2019-02-28 17:08:49 -0500

    Add files via upload

---
+++ pineapple/pineapple-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  pineapple-Bridging-Header.h
+//  pineapple
+//
+//  Created by litchi on 2/28/19.
+//  Copyright © 2019 bose.asd. All rights reserved.
+//
+
+#ifndef pineapple_Bridging_Header_h
+#define pineapple_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* pineapple_Bridging_Header_h */
